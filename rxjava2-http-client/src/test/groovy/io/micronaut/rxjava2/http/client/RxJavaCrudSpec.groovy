05eba5047bb10804b02a900e31888c9b9a92793b
Sergio del Amo <sergio.delamo@softamo.com>
2021-07-07 16:45:11 +0200

    test: add RxJavaCrudSpec from rxjava3 repo

---
+++ rxjava2-http-client/src/test/groovy/io/micronaut/rxjava2/http/client/RxJavaCrudSpec.groovy
@@ -0,0 +1,192 @@
+package io.micronaut.rxjava2.http.client
+
+import io.micronaut.context.annotation.Property
+import io.micronaut.context.annotation.Requires
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.annotation.*
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import io.reactivex.Flowable
+import io.reactivex.Maybe
+import io.reactivex.Single
+import jakarta.inject.Inject
+import spock.lang.Specification
+import java.util.concurrent.atomic.AtomicLong
+
+/**
+ * @author graemerocher
+ * @since 1.0
+ */
+@Property(name = 'spec.name', value = 'RxJavaCrudSpec')
+@MicronautTest
+class RxJavaCrudSpec extends Specification {
+
+    @Inject
+    BookClient client
+
+    @Inject
+    @Client("/")
+    RxHttpClient rxHttpClient
+
+    void "test it is possible to implement CRUD operations with RxJava"() {
+
+        when:
+        def result = client.get(99)
+                .blockingGet()
+        Book book = result
+        List<Book> books = client.list().blockingGet()
+
+
+        then:
+        book == null
+        books.size() == 0
+        client.flow().toList().blockingGet().size() == 0
+
+        when:
+        book = client.save("The Stand").blockingGet()
+
+        then:
+        book != null
+        book.title == "The Stand"
+        book.id == 1
+
+        when:
+        book = client.get(book.id).blockingGet()
+
+        then:
+        book != null
+        book.title == "The Stand"
+        book.id == 1
+        client.flow().toList().blockingGet().size() == 1
+        rxHttpClient.retrieve(HttpRequest.GET("/rxjava/books/" + book.id), Book).blockingFirst()
+                .title == "The Stand"
+
+
+        when:'the full response is resolved'
+        HttpResponse<Book> bookAndResponse = client.getResponse(book.id).blockingGet()
+
+        then:"The response is valid"
+        bookAndResponse.status() == HttpStatus.OK
+        bookAndResponse.body().title == "The Stand"
+
+        when:
+        book = client.update(book.id, "The Shining").blockingGet()
+
+        then:
+        book != null
+        book.title == "The Shining"
+        book.id == 1
+
+        when:
+        book = client.delete(book.id).blockingGet()
+
+        then:
+        book != null
+
+        when:
+        book = client.get(book.id)
+                .blockingGet()
+        then:
+        book == null
+    }
+
+    @Requires(property = 'spec.name', value = 'RxJavaCrudSpec')
+    @Client('/rxjava/books')
+    static interface BookClient extends BookApi {
+    }
+
+    @Requires(property = 'spec.name', value = 'RxJavaCrudSpec')
+    @Controller("/rxjava/books")
+    static class BookController implements BookApi {
+
+        Map<Long, Book> books = new LinkedHashMap<>()
+        AtomicLong currentId = new AtomicLong(0)
+
+        @Override
+        Maybe<Book> get(Long id) {
+            Book book = books.get(id)
+            if(book)
+                return Maybe.just(book)
+            Maybe.empty()
+        }
+
+        @Override
+        Single<HttpResponse<Book>> getResponse(Long id) {
+            Book book = books.get(id)
+            if(book) {
+                return Single.just(HttpResponse.ok(book))
+            }
+            return Single.just(HttpResponse.notFound())
+        }
+
+        @Override
+        Single<List<Book>> list() {
+            return Single.just(books.values().toList())
+        }
+
+        @Override
+        Flowable<Book> flow() {
+            return Flowable.fromIterable(books.values())
+        }
+
+        @Override
+        Maybe<Book> delete(Long id) {
+            Book book = books.remove(id)
+            if(book) {
+                return Maybe.just(book)
+            }
+            return Maybe.empty()
+        }
+
+        @Override
+        Single<Book> save(String title) {
+            Book book = new Book(title: title, id:currentId.incrementAndGet())
+            books[book.id] = book
+            return Single.just(book)
+        }
+
+        @Override
+        Maybe<Book> update(Long id, String title) {
+            Book book = books[id]
+            if(book != null) {
+                book.title = title
+                return Maybe.just(book)
+            }
+            else {
+                return Maybe.empty()
+            }
+        }
+    }
+
+    static interface BookApi {
+
+        @Get("/{id}")
+        Maybe<Book> get(Long id)
+
+        @Get("/res/{id}")
+        Single<HttpResponse<Book>> getResponse(Long id)
+
+        @Get
+        Single<List<Book>> list()
+
+        @Get("/flow")
+        Flowable<Book> flow()
+
+        @Delete("/{id}")
+        Maybe<Book> delete(Long id)
+
+        @Post
+        Single<Book> save(String title)
+
+        @Patch("/{id}")
+        Maybe<Book> update(Long id, String title)
+    }
+
+
+    static class Book {
+        Long id
+        String title
+    }
+}
