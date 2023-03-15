ee0bc94046176f5e0685344e151b4d897e017088
Dean Wette <wetted@objectcomputing.com>
2023-03-15 05:30:12 -0500

    build: plugin 6.3.5, gradle 8.0.2, jakarta validation, standardized project names (#132)

--- rxjava2-http-client/src/test/groovy/io/micronaut/rxjava2/http/client/RxHttpPostSpec.groovy
+++ rxjava2-http-client/src/test/groovy/io/micronaut/rxjava2/http/client/RxHttpPostSpec.groovy
@@ -43,8 +43,8 @@ import spock.lang.IgnoreIf
 import spock.lang.Shared
 import spock.lang.Specification
 
-import javax.validation.Valid
-import javax.validation.constraints.NotNull
+import jakarta.validation.Valid
+import jakarta.validation.constraints.NotNull
 
 /**
  * @author Graeme Rocher
